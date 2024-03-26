a98102b5aa351135a864f335cd82e6cfcb6f598a
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-03-25 21:03:53 -0700

    inital commit, stting up

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Freight Flow</h1>
+      </header>
+      <main>
+        {/*  main content here */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="main-content">
+      <h2>Welcome to Freight Flow</h2>
+      <p>This is the home page content.</p>
+    </div>
+  );
+}
+
+export default Home;
