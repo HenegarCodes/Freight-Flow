56a1d2ddc37e3b5c67dfb53e30feaa2352bb4e4e
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-07-05 16:50:03 -0700

    adding initial file setup for signup and login :

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
-import Header from'./components/Header.jsx'
-import Home from './pages/Home.jsx'
+import Header from './components/Header';
+import Home from './pages/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
- 
+      <Router>
         <Header />
- 
-      <main>
-        <Home />
-        {/*  main content here */}
-      </main>
+        <main>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/dashboard" component={Dashboard} />
+          </Switch>
+          {/* Add login/signup buttons here */}
+          <div>
+            <Link to="/login"><button>Login</button></Link>
+            <Link to="/signup"><button>Signup</button></Link>
+          </div>
+        </main>
+      </Router>
     </div>
   );
 }
