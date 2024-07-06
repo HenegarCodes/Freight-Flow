a6a605a988480e273a2cb8a010572caba25e0079
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-07-05 17:21:18 -0700

    Adding the fixed links to redirect upon clicking on homepage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import './header.css'
+import { Link } from 'react-router-dom';
+import './header.css';
 
 function Header() {
   return (
-<section className='nav'>
-    <div className='logo'>Freight Flow</div>
+    <section className='nav'>
+        <div className='logo'>Freight Flow</div>
 
-    <input id='menu-toggle' type="checkbox" />
-    <label className='menu-button-container' for="menu-toggle">
-        <div className='menu-button'></div>
-    </label>
-    <ul className='menu'>
-        <li>Dashboard</li>
-        <li>Map</li>
-        <li>User</li>
-    </ul>
+        <input id='menu-toggle' type="checkbox" />
+        <label className='menu-button-container' htmlFor="menu-toggle">
+            <div className='menu-button'></div>
+        </label>
+        <ul className='menu'>
+            <li><Link to="/dashboard">Dashboard</Link></li>
+            <li><Link to="/map">Map</Link></li>
+            <li><Link to="/user">User</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+        </ul>
 
-</section>
+    </section>
   );
 }
 
