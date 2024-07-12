8025436a58e50729a96ad6cfa9e210039f4b4e53
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-07-11 19:40:44 -0700

    adding string for mongodb. security issue but needed to do sso to fix it in env

--- server/app.js
+++ server/app.js
@@ -4,15 +4,19 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json()); // Middleware to parse JSON bodies
 
+console.log(process.env.MONGO_URI); // This should output your MongoDB URI
+
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://henegarlearnscode:Leishy415!@freightflow.j1xkvdl.mongodb.net/?retryWrites=true&w=majority&appName=FreightFlow")
   .then(() => console.log("MongoDB connected successfully"))
   .catch(err => console.log(err));
+
+
 
 // Define routes
 app.get('/', (req, res) => {
