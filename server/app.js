44d921c79aba077a37d9a9a6859986e026aabd5f
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-09-30 14:50:13 -0700

    fixing port issue

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors()); // Apply CORS middleware
