78e34e66c0faa483c01e9a703471f64c40dd3f54
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2025-01-10 11:32:57 -0700

    fixing another import

--- src/client/src/components/User.jsx
+++ src/client/src/components/User.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './User.css';
+import './user.css';
 
 const User = () => {
   const [userData, setUserData] = useState({});
