6f1eb94d583c25e7c4cd4486aee2a5b972aaf1e2
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2025-01-10 11:27:34 -0700

    fixing routes

--- src/server/routes/authRoutes.js
+++ src/server/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
-const verifyToken = require('../middleware/authmiddlewares'); 
+const verifyToken = require('../middleware/authMiddlewares'); 
 const router = express.Router();
 
 // Signup Route
