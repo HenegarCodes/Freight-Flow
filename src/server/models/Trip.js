ba539e6381701677a51bfc21ee379b14798e2aa2
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2025-01-25 12:51:02 -0700

    ....

--- src/server/models/Trip.js
+++ src/server/models/Trip.js
@@ -2,12 +2,22 @@ const mongoose = require('mongoose');
 
 const TripSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  start: { type: Object, required: true },
+  start: { type: String, required: true },
   end: { type: String, required: true },
   stops: { type: [String], default: [] },
-  truckHeight: { type: String },
-  truckWeight: { type: String },
-  route: { type: Object, required: true },
-}, { timestamps: true });
+  route: {
+    distance: { type: String },
+    duration: { type: String },
+    waypoints: [
+      {
+        start: { type: Object },
+        end: { type: Object },
+        instructions: { type: String },
+      },
+    ],
+  },
+  createdAt: { type: Date, default: Date.now },
+});
 
 module.exports = mongoose.model('Trip', TripSchema);
+
