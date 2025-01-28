4119c41203ab0d1003e6df4f0d570d35230ccdb8
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2025-01-28 13:01:47 -0700

    adding

--- src/server/app.js
+++ src/server/app.js
@@ -14,10 +14,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
-app.use(express.json({ limit: '50mb' })); // Adjust 
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
+app.use(express.json({ limit: '15mb' })); // Adjust 
+app.use(express.urlencoded({ extended: true, limit: '15mb' }));
+app.use(express.json());
 // Register routes
 app.use('/api/auth', authRoutes);
 app.use('/api/trips', tripsRoute); 
