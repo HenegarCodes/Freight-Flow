826a50876475557c16ba5044bc2e1d53ced895eb
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2025-01-28 00:17:21 -0700

    fixing payload

--- src/server/app.js
+++ src/server/app.js
@@ -15,6 +15,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.json({ limit: '50mb' })); // Adjust 
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Register routes
 app.use('/api/auth', authRoutes);
